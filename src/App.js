937e100b30274d2ed42e28410f1f1fd8cd9bc106
m.renaud <m.renaud@everwin.fr>
2020-03-16 13:09:31 +0100

    "configuration to run with postgresql"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  const [data, setData] = useState({ users: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+          'http://localhost:8083/user', { headers: { "Authorization": "Bearer " + localStorage.getItem("react-token") } }
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {console.log(data)}
+        <header className="App-header">
+          <h1>Secure React App</h1>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div>
+
+            <h2>Response from Quarkus API: /user </h2>
+
+            <p>Name: {data.name}</p>
+            <p>Email: {data.email}</p>
+
+          </div>
+        </header>
+      </div>
   );
 }
 
